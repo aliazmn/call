41189d34b8ad32d6427cdbe31b1d31f8309a5ee8
ali <aliazamian1@gmail.cpm>
2022-05-25 15:38:57 +0430

    s

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, Response
+
+app = Flask(__name__)
+
+
+@app.route("back/")
+def callback():
+    with open("data.txt", "w") as f:
+        f.write(f"data:{request.data} , query: {request.query_string}")
+
+    return Response("done !")
