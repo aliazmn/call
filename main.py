b0c07de36944417382a823c77d6f0aa7bf41f246
ali <aliazamian1@gmail.cpm>
2022-05-25 15:43:35 +0430

    asd

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import Flask, request, Response
 app = Flask(__name__)
 
 
-@app.route("back/")
+@app.route("/back/")
 def callback():
     with open("data.txt", "w") as f:
         f.write(f"data:{request.data} , query: {request.query_string}")
