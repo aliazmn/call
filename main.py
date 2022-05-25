ea62e4427a99c5f48b8fb33bbf4f942f6f97dbef
ali <aliazamian1@gmail.cpm>
2022-05-25 15:59:01 +0430

    asd

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from flask import Flask, request, Response
 app = Flask(__name__)
 
 
-@app.route("/back/")
+@app.route("/back")
 def callback():
-    with open("data.txt", "w") as f:
+    with open("./data.txt", "w") as f:
         f.write(f"data:{request.data} , query: {request.query_string}")
 
     return Response("done !")
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=6566)
+    app.run(host="0.0.0.0", port=8001)
